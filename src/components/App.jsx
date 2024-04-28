b4a1687332877e3590a3077cc666941cdf81a12b
Dharmendra <jatdharmendra67@gmail.com>
2024-04-28 12:23:56 +0530

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,101 +1,129 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 function App() {
 
 
     const questions = [
         {
-            
-            questionText: "Which type of JavaScript language is ___",
-            answerOptions: [
-                { answerText: 'Object - Oriented', isCorrect: false },
-                { answerText: 'Object - Based', isCorrect: true },
-                { answerText: 'Assembly - language', isCorrect: false },
-                { answerText: 'High - level', isCorrect: false },
-            ],
+            id: 1,
+            question: "Which language runs in a web browser?",
+            options: ["Java", "C", "Python", "JavaScript"],
+            correctAnswer: "JavaScript"
         },
         {
-            
-            questionText: "Which one of the following also known as Conditional Expression: ",
-            answerOptions: [
-                { answerText: 'Alternative to if-else', isCorrect: false },
-                { answerText: 'Switch statement', isCorrect: false },
-                { answerText: 'If-then-else statement', isCorrect: false },
-                { answerText: 'immediate if', isCorrect: true },
-            ],
+            id: 2,
+            question: "What does CSS stand for?",
+            options: ["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+            correctAnswer: "Cascading Style Sheets"
         },
         {
-        
-            questionText: "In JavaScript, what is a block of statement? ",
-            answerOptions: [
-                { answerText: 'Conditional block', isCorrect: false },
-                { answerText: 'Switch statement', isCorrect: false },
-                { answerText: 'If-then-else statement', isCorrect: false },
-                { answerText: 'immediate if', isCorrect: true },
-            ],
+            id: 3,
+            question: "What does HTML stand for?",
+            options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+            correctAnswer: "Hypertext Markup Language"
         },
         {
-            
-            questionText: "When interpreter encounters an empty statements, what it will do: ",
-            answerOptions: [
-                { answerText: 'Shows a warning', isCorrect: false },
-                { answerText: 'Prompts to complete the statement', isCorrect: true },
-                { answerText: 'Throws an error', isCorrect: false },
-                { answerText: 'Ignores the statements', isCorrect: false },
-            ],
-        }
+            id: 4,
+            question: "What year was JavaScript launched?",
+            options: ["1996", "1995", "1994", "none of the above"],
+            correctAnswer: "none of the above"
+        },
     ];
+
     const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [showScore ,setShowScore]=useState(false);
-const[score,setScore]=useState(0);
-    const handelChange = (isCorrect) => {
-        if(isCorrect===true){
-            setScore(score + 1);
-        }}
-        function onClickNext(){
-        const nextQuestion = currentQuestion+1;
-        if (questions.length > nextQuestion) {
-            setCurrentQuestion(nextQuestion);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
+    const [selectedOptions, setSelectedOptions] = useState(Array(questions.length).fill(null));
+    const [answered, setAnswered] = useState(Array(questions.length).fill(false));
+    const [prevSelectedOptions, setPrevSelectedOptions] = useState(Array(questions.length).fill(null));
+
+
+
+    const handleAnswerClick = (selectedOption) => {
+        const newSelectedOptions = [...selectedOptions];
+        newSelectedOptions[currentQuestion] = selectedOption;
+        setSelectedOptions(newSelectedOptions);
+        setAnswered(answered.map((value, index) => index === currentQuestion ? true : value));
+        updateScore(selectedOption);
+      };
+      
+      const updateScore = (selectedOption) => {
+        const prevSelectedOption = prevSelectedOptions[currentQuestion];
+      
+        if (selectedOption === questions[currentQuestion].correctAnswer && prevSelectedOption !== questions[currentQuestion].correctAnswer) {
+          setScore(score + 1);
+        } else if (selectedOption !== questions[currentQuestion].correctAnswer && prevSelectedOption === questions[currentQuestion].correctAnswer) {
+          setScore(score - 1);
+        }
+      
+        const newPrevSelectedOptions = [...prevSelectedOptions];
+        newPrevSelectedOptions[currentQuestion] = selectedOption;
+        setPrevSelectedOptions(newPrevSelectedOptions);
+      };
+
+
+    function onClickNext() {
+        if (questions.length - 1 > currentQuestion) {
+            setCurrentQuestion(currentQuestion + 1);
+
         }
         else {
-            setShowScore(true);
+            setShowScore(true)
         }
-    }
-    function onClickPrevious(){
-        const previousQuestion = currentQuestion-1 ;
-        if (questions.length > previousQuestion) {
-            setCurrentQuestion(previousQuestion);
-        } 
-        else{
-           alert("no more questions")
+    };
+
+
+    function onClickPrevious() {
+
+        if (currentQuestion > 0) {
+            setCurrentQuestion(currentQuestion - 1);
+
         }
+
     }
+
+
 
     return (
         <div className="app">
-        {showScore ? (
-<div className="score-section ">Your Score is {score} out of {questions.length} </div>):(
-    <>  <div>
-                <div className="question-count"><span>Question {currentQuestion+1}</span>/ {questions.length}</div>
+            {showScore ? (
+                <div className="score-section ">Your Score is {score} out of {questions.length} </div>) : (
+                <>  <div>
+                    <div className="question-count"><span>Question {currentQuestion + 1}</span>/
+                        {questions.length}</div>
 
-                <div className="question-text"> {questions[currentQuestion].questionText} </div>
+                    <div className="question-text"> {questions[currentQuestion].question} </div>
 
-                <div className="answer-section"> {questions[currentQuestion].answerOptions.map((answer) => {
-                    return (<button onClick={()=>handelChange(answer.isCorrect)}>{answer.answerText}</button>);
-                }) } 
-                <div className="button-next">  <button className="next" onClick={onClickNext}>Next </button> </div>
-                <div className="button-pre"><button className="next" onClick={onClickPrevious}> Previous </button></div>
-               </div>
-            </div>
-            </>
-)
+                    <div className="answer-section">
+                        {questions[currentQuestion].options.map((option, index) => (
 
-        }
-            
-            
+                            <button key={index}
+                                onClick={() => handleAnswerClick(option)}
+                                className={`button-option ${ answered[currentQuestion] && 
+                                (selectedOptions[currentQuestion] === option ?
+                                 "selected" : null)}`}>
+                                {option}</button>)
+                        )}
+
+
+                        <div className="buttons-app">
+                            <div className="button-pre"><button className="next"
+                                onClick={onClickPrevious} disabled={currentQuestion === 0}>
+                                Previous </button></div>
+                            <div className="button-next">  <button className="next"
+                                onClick={onClickNext} >Next </button> </div>
+
+                        </div>
+                    </div>
+                </div>
+                </>
+
+            )
+            }
         </div>
-        
+
     );
+
 
 }
 export default App;
